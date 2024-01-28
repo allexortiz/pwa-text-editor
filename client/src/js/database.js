a58c5ca4a698b8e743b6ba90fd0747e1bdf333ab
Allex Ortiz <allexortiz@outlook.com>
2024-01-28 16:34:17 -0600

    added the ability to put and get data from the IndexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,53 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Function to put data into the database
+export const putDb = async (content) => {
+  console.log('PUT to the database'); // Log to indicate putting data into the database
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Open the database named 'jate' with version 1
+  const contactDb = await openDB('jate', 1);
+
+  // Start a transaction in read-write mode
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Access the object store named 'jate'
+  const store = tx.objectStore('jate');
+
+  // Put the data into the object store with an ID of 1
+  const request = store.put({ id: 1, value: content });
+
+  // Wait for the request to complete
+  const result = await request;
+
+  // Log the success message along with the result
+  console.log('data saved to the database', result);
+};
+
+// Function to get data from the database
+export const getDb = async () => {
+  console.log('GET from the database'); // Log to indicate getting data from the database
+
+  // Open the database named 'jate' with version 1
+  const contactDb = await openDB('jate', 1);
+
+  // Start a transaction in read-only mode
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  // Access the object store named 'jate'
+  const store = tx.objectStore('jate');
+
+  // Get all data from the object store
+  const request = store.getAll();
+
+  // Wait for the request to complete
+  const result = await request;
+
+  // Log the retrieved data
+  console.log('result.value', result);
+
+  // Return the retrieved data (value property of result)
+  return result?.value;
+};
 
 initdb();
